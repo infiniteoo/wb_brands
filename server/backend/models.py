801f78d1add28f05ce9fcdde0f04b27510be723c
infiniteoo <troydorman@gmail.com>
2023-12-08 20:03:26 -0700

    added mongodb connection

--- server/backend/models.py
+++ server/backend/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Brand(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
+    image = models.CharField(max_length=1000)
+    category = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
--- server/server/settings.py
+++ server/server/settings.py
@@ -11,6 +11,29 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+import pymongo
+
+load_dotenv()
+
+MONGO_URI = os.getenv("MONGO_URI")
+print(f'MONGI URI: {MONGO_URI}')
+
+try:
+    # Create a MongoDB client instance
+    mongo_client = pymongo.MongoClient(MONGO_URI)
+
+    # Attempt to connect to MongoDB
+    mongo_client.server_info()  # This will trigger a connection attempt
+
+    # If successful, print a success message
+    print("Successfully connected to MongoDB.")
+except pymongo.errors.ServerSelectionTimeoutError as e:
+    # Handle connection failure and print an error message
+    print("Failed to connect to MongoDB: ", e)
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,6 +62,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
+  
 ]
 
 MIDDLEWARE = [
@@ -73,15 +97,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'server.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
